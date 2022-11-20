44ab7b715174a32659ee81aeeeaadc5e1541e870
Divyansh Goyal <divyansh.goyal2001@gmail.com>
2022-11-19 18:16:22 -0700

    Fixed landing page slightly

--- code-sign/src/Components/Principles.js
+++ code-sign/src/Components/Principles.js
@@ -1,12 +1,23 @@
 import "./Principles.css";
 import { Container, Row, Col } from "react-bootstrap";
 import { useInView } from "react-intersection-observer";
+import graph from "../Images/graph.png";
 
 function Principles() {
   return (
     <Container fluid className="principlesParent">
       <Container fluid className="principles" id="principles">
-        <Row></Row>
+        <Row className="text-center" style={{ margin: "auto" }}>
+          <Col>
+            <img src={graph} width={"100%"} />
+          </Col>
+          <Col>
+            <img src={graph} width={"100%"} />
+          </Col>
+          <Col>
+            <img src={graph} width={"100%"} />
+          </Col>
+        </Row>
       </Container>
     </Container>
   );
