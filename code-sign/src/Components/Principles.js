ae5da1cb65e1999c3b58c8d534177de7eb6ad689
anisin22 <54185231+anisin22@users.noreply.github.com>
2022-11-20 17:43:25 -0500

    Started on principle page

--- code-sign/src/Components/Principles.js
+++ code-sign/src/Components/Principles.js
@@ -2,21 +2,17 @@ import "./Principles.css";
 import { Container, Row, Col } from "react-bootstrap";
 import { useInView } from "react-intersection-observer";
 import graph from "../Images/graph.png";
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
 
 function Principles() {
   return (
     <Container fluid className="principlesParent">
       <Container fluid className="principles" id="principles">
         <Row className="text-center" style={{ margin: "auto" }}>
-          <Col>
-            <img src={graph} width={"100%"} />
-          </Col>
-          <Col>
-            <img src={graph} width={"100%"} />
-          </Col>
-          <Col>
-            <img src={graph} width={"100%"} />
-          </Col>
+        <Row className="title">
+          <h1 className="principleTitle">What is Most Important to Us</h1>
+        </Row>
+        <div class="rectangle"></div>
         </Row>
       </Container>
     </Container>
